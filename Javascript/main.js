225428d41aed58844b203b069020a4e59b3d6ead
Jackeline <jackelineandrea58@hotmail.com>
2021-03-24 16:07:19 +0000

    improved

--- Javascript/main.js
+++ Javascript/main.js
@@ -23,13 +23,38 @@ document.addEventListener('DOMContentLoaded', async() => {
     const upcomingAnimes = [...springAnimes, ...summerAnimes]
     console.log(upcomingAnimes)
 
-    upcomingAnimes.forEach(animeInList => {
-       
-  
+    upcomingAnimes.forEach(animeInList => {  
         const informationPrint = document.querySelector('.wallpaper');
-
         const animeWallpapers = animeInList.image_url;
         informationPrint.innerHTML += ` <img src="${animeWallpapers}"> `
 
+    // function displayDesiredInformation() {
+    //     const dataContainer = document.querySelector('.text')
+    //     dataContainer.innerHTML = ` <ul> 
+    //     <h3> ${animeInList.title} </h3>
+    //     <p> Synopsis: ${animeInList.synopsis}. </p>
+    //     <p> Start date: ${animeInList.airing_start} </p>
+    //     <p> Type: ${animeInList.type} - Episodes: ${animeInList.episodes} </p>
+    //     <p> Click <a href="${animeInList.url}" here </a> for more information. </p>
+    //     </ul> `
+    //     }
+    })
+
+    const animePictures = document.getElementsByTagName("img");
+    for (i= 0; i < animePictures.length; i++) {
+        animePictures[i].className += "anime-pics"
+    }
+
+    //console.log(animePictures)
+    const wallpaperAnime = [...animePictures]; // Array of Pictures.
+    console.log(wallpaperAnime);
+
+    wallpaperAnime.forEach(pictureInPage => {
+        pictureInPage.addEventListener('click', function (event) {
+            console.log("You clicked me.")
+            //displayDesiredInformation();
+        const dataContainer = document.querySelector('.text')
+        dataContainer.innerHTML = `<ul> Hello </ul>`
+        })
     })
 })
